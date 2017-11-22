a82a08b88818e78f913c703fcccafe16d79aed87
John Coleman <john.coleman@yale.edu>
2017-11-21 21:11:09 -0500

    split default and laptop recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,17 +3,3 @@
 # Recipe:: default
 #
 include_recipe 'yum'
-
-yum_repository 'dropbox' do
-  description "Dropbox repo"
-  baseurl "http://linux.dropbox.com/fedora/25"
-  gpgkey 'https://linux.dropbox.com/fedora/rpm-public-key.asc'
-  action :create
-end
-
-yum_repository 'virtualbox' do
-  description "Fedora $releasever - $basearch - VirtualBox"
-  baseurl "http://download.virtualbox.org/virtualbox/rpm/fedora/$releasever/$basearch"
-  gpgkey 'https://www.virtualbox.org/download/oracle_vbox.asc'
-  action :create
-end
